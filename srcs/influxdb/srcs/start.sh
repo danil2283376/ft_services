24f375c1f02940b1395bf71d928037952e447d7f
Spawn Colen <scolen@pr-j2.kzn.21-school.ru>
2021-04-21 17:01:02 +0300

    up grafana

--- srcs/influxdb/srcs/start.sh
+++ srcs/influxdb/srcs/start.sh
@@ -1,2 +1,6 @@
-/etc/init.d/telegraf start
-/usr/sbin/influxd -config /etc/influxdb.conf
+/usr/bin/supervisord -c /etc/supervisord.conf
+
+# /etc/init.d/telegraf start
+influx -execute "CREATE DATABASE influxdb"
+influx -execute "CREATE USER admin WITH PASSWORD 'admin'"
+# /usr/sbin/influxd -config /etc/influxdb.conf
