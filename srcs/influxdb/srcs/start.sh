2ccb13d5e36ef2b00a23efa0b3372a03f73ba4bb
Spawn Colen <scolen@un-e2.kzn.21-school.ru>
2021-04-24 19:39:23 +0300

    rm trash

--- srcs/influxdb/srcs/start.sh
+++ srcs/influxdb/srcs/start.sh
@@ -1,8 +1,2 @@
-
-# /etc/telegraf start
-# influx -execute "CREATE DATABASE influxdb"
-# influx -execute "CREATE USER admin WITH PASSWORD 'admin'"
-# /usr/bin/supervisord -c /etc/supervisord.conf
-# /usr/sbin/influxd -config /etc/influxdb.conf
 /etc/init.d/telegraf start
 /usr/sbin/influxd -config /etc/influxdb.conf
